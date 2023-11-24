2e6f78eefeccbf6560033a222831d61076fd0672
daisuke-ishigure <83412124+daisuke-ishigure@users.noreply.github.com>
2023-11-25 01:10:09 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 $();
 
 const jsonAddress = "../js/hyakunin.json";
@@ -10,23 +10,21 @@ let poems = [];
 let selectedTheme = "";
 
 // テーマが変更されたら、設定値をselectedThemeに格納する
-$('#themeSelector').on('change', function () {
+$("#themeSelector").on("change", function () {
   selectedTheme = $(this).val();
   console.log(selectedTheme);
   // テーマによって、歌人をソートする
   updateJsonSelectorOptions();
 });
 
-
 // JSONデータを取得する
 fetch(jsonAddress)
   .then((response) => response.json())
   .then((data) => {
     poems = data;
-
   })
   .catch((error) => {
-    console.error('データの取得に失敗!:', error);
+    console.error("データの取得に失敗!:", error);
   });
 
 // 選択されたテーマの歌人を抽出する
@@ -45,62 +43,74 @@ function getThemePoems() {
 // 選択されたテーマの歌人を抽出する
 function updateJsonSelectorOptions() {
   let themePoems = getThemePoems();
-  $('#jsonSelector').html(
-    '<option value="all">歌人を選んでください</option>'
-  );
+  $("#jsonSelector").html('<option value="all">歌人を選んでください</option>');
   themePoems.forEach(function (poem) {
-    let strippedName = poem.name.replace(/<rt>.*?<\/rt>/g, '');/* rt要素を削除する */
-    $('#jsonSelector').append(
-      `<option value="${poem.number}">第${poem.number}首:${strippedName}</option>`
+    let strippedName = poem.name.replace(
+      /<rt>.*?<\/rt>/g,
+      ""
+    ); /* rt要素を削除する */
+    $("#jsonSelector").append(
+      `<option value="${poem.number}">第${poem.number}番・${strippedName}</option>`
     );
   });
 }
 
+// ----------------------------------------------
+// クリック動作で札を裏返すトグル
+// ----------------------------------------------
 $(function () {
-  $('.card-item').on('click', function (event) {
-    $(this).toggleClass('active');
+  $(".card-item").on("click", function (event) {
+    $(this).toggleClass("active");
     event.stopPropagation();
-  })
-})
+  });
+});
 
-
+// ----------------------------------------------
+// スイッチで札を裏返すトグル
+// ----------------------------------------------
+$("#toggle2").on("change", function (event) {
+  $(".card-item").toggleClass("active");
+  event.stopPropagation();
+});
 
 jsonSelector.addEventListener("change", () => {
   const selectedDataSet = jsonSelector.value;
 
   //札を表に返す
-  if ($('.card-item').hasClass('active')) {
-    $('.card-item').removeClass('active');
-  }
+  // if ($('.card-item').hasClass('active')) {
+  //   $('.card-item').removeClass('active');
+  // }
 
   //表に要素があれば削除する
-  if ($('.card-front').children().length > 0) {/* '.card-front' 要素に子要素がある場合 */
-    $('.card-front').empty(); /* '.card-front' 要素の子要素をすべて削除 */
+  if ($(".card-front").children().length > 0) {
+    /* '.card-front' 要素に子要素がある場合 */
+    $(".card-front").empty(); /* '.card-front' 要素の子要素をすべて削除 */
   }
   //表の写真に要素があれば削除する
-  if ($('.photo').children().length > 0) {
-    $('.photo').empty(); // photoの子要素をすべて削除する
+  if ($(".photo").children().length > 0) {
+    $(".photo").empty(); // photoの子要素をすべて削除する
   }
   //  //裏に要素があれば削除する
-  if ($('.card-back').children().length > 0) {
+  if ($(".card-back").children().length > 0) {
     // containerBackに子要素があれば
-    $('.card-back').empty(); // containerBackの子要素をすべて削除する
+    $(".card-back").empty(); // containerBackの子要素をすべて削除する
   }
-  if ($('.explanation').children().length > 0) {
+  if ($(".explanation").children().length > 0) {
     // containerBackに子要素があれば
-    $('.explanation').empty(); // containerBackの子要素をすべて削除する
+    $(".explanation").empty(); // containerBackの子要素をすべて削除する
   }
-
-
 
   fetch(jsonAddress)
     .then((response) => response.json())
-    .then((data) => {/* jsonデータを取り込んだときの動作ここから */
+    .then((data) => {
+      /* jsonデータを取り込んだときの動作ここから */
       const selectedData = data[selectedDataSet];
       const yomihuda = selectedData.yomihuda;
+      const torihuda = selectedData.torihuda;
       const first = selectedData.first;
       const second = selectedData.second;
       const name = selectedData.name;
+      const date = selectedData.date
       const nameKana = selectedData.nameKana;
       const theme = selectedData.theme;
       const translation = selectedData.translation;
@@ -109,7 +119,7 @@ jsonSelector.addEventListener("change", () => {
       const personality = selectedData.personality;
 
       // HTMLを更新
-      $('.card-front').append(`
+      $(".card-front").append(`
       <dl>
         <dt id="poemName">${name}</dt>
         <dd id="poemContent">${first}<br>${second}</dd>
@@ -117,82 +127,117 @@ jsonSelector.addEventListener("change", () => {
       <img src="../img/${number}.svg" alt="${name}">
       `);
 
-// ---------------------------------------------------
-// ひらがなトグルボタンについて
-// ---------------------------------------------------
+      // ---------------------------------------------------
+      // ひらがなトグルボタンについて
+      // ---------------------------------------------------
 
-const checkbox = document.getElementById('toggle');
-  let startX;
+      const checkbox = document.getElementById("toggle2");
+      let startX;
 
-  checkbox.addEventListener('touchstart', (e) => {
-    startX = e.touches[0].clientX;
-  });
+      checkbox.addEventListener("touchstart", (e) => {
+        startX = e.touches[0].clientX;
+      });
 
-  checkbox.addEventListener('touchmove', (e) => {
-    const currentX = e.touches[0].clientX;
-    const deltaX = currentX - startX;
+      checkbox.addEventListener("touchmove", (e) => {
+        const currentX = e.touches[0].clientX;
+        const deltaX = currentX - startX;
 
-    if (deltaX > 10) {
-      // Swiped to the right, toggle checkbox to checked
-      checkbox.checked = true;
-      updatePoemContent();
-      updateNameContent();
-    } else if (deltaX < -10) {
-      // Swiped to the left, toggle checkbox to unchecked
-      checkbox.checked = false;
-      updatePoemContent();
-      updateNameContent();
-    }
-  });
+        if (deltaX > 10) {
+          // Swiped to the right, toggle checkbox to checked
+          checkbox.checked = true;
+          updatePoemContent();
+          updateNameContent();
+        } else if (deltaX < -10) {
+          // Swiped to the left, toggle checkbox to unchecked
+          checkbox.checked = false;
+          updatePoemContent();
+          updateNameContent();
+        }
+      });
 
-
-
-// スマホのチェックボックスのスワイプ対応ここまで
-
-
+      // スマホのチェックボックスのスワイプ対応ここまで
       // チェックボックスの変更時にコンテンツを即座に更新
-      $('#toggle').on('change', function () {
+      $("#toggle3").on("change", function () {
         updatePoemContent();
-        updateNameContent();
+        // updateNameContent();
       });
 
       // 初回読み込み時にも更新
       updatePoemContent();
-      updateNameContent();
 
-      // 選択されたデータを表示する関数
+      /// 下の句を隠すチェックボックス
       function updatePoemContent() {
-        const poemContent = $('#toggle').is(':checked') ? yomihuda : `${first}<br>${second}`;
-        /* ?はif elseの意味 */
-        $('#poemContent').html(poemContent);
+        // '#toggle0'という要素がチェックされているかどうかを確認します
+        const isToggleChecked = $("#toggle3").is(":checked");
+        // チェックされているかどうかを反転させます
+        const isToggleUnchecked = !isToggleChecked;
+        // チェックが外れたらline-heightを3に、チェックが付いたらline-heightを1.95に変更
+        const lineheightValue = isToggleUnchecked ? "3" : "1.95";
+
+        // line-heightの変更をtransitionでスムーズにする
+        $(".card-front dl dd").css({
+          "line-height": lineheightValue,
+          transition: "line-height 0.1s ease-in-out",
+        });
+
+        // 反転した状態を使用して詩のコンテンツを更新します
+        const poemContent = isToggleUnchecked
+          ? `${first}`
+          : `${first}<br>${second}`;
+
+        // '#poemContent'という要素のHTMLコンテンツを更新します
+        $("#poemContent").html(poemContent);
       }
+
+      // チェックボックスの変更があった場合にupdatePoemContent関数を呼び出す
+      $("#toggle0").change(updatePoemContent);
 
       function updateNameContent() {
-        const poemName = $('#toggle').is(':checked') ? nameKana : `${name}`;
+        const poemName = !$("#toggle1").is(":checked") ? nameKana : `${name}`;
+
         /* ?はif elseの意味 */
-        $('#poemName').html(poemName);
+        $("#poemName").html(poemName);
       }
 
-      $('.card-back').append(`
-      <dl>
-      <dt>歌の意味</dt>
-      <dd>${translation}</dd>
-      </dl>
+      $(".card-back").append(`
+      <p>
+      ${torihuda}
+      </p>
        `);
-      $('.explanation').append(`
+
+
+
+
+
+
+      $(".explanation").append(`
+       <dl>
+       <dt>和歌</dt>
+       <dd>${first.replace(/<br\s*\/?>/g, " ") }
+       <br>${second.replace(/<br\s*\/?>/g, " ") }<br>
+       <span class="small">${name}（${date}）</span>
+    </dd>
+        </dl>
+        <dl>
+        <dt>現代語訳</dt>
+        <dd>${translation}</dd>
+      </dl>
+
       <dl>
-        <dt>歌について</dt>
+        <dt>歌が詠まれた背景</dt>
         <dd>${bg}</dd>
       </dl>
       <dl>
         <dt>歌人の人となり</dt>
         <dd>${personality}</dd>
       </dl>
-      `)
-      $('#sp_menu').append(`
+      `);
+      $("#sp_menu").append(`
         <table>
-      `)
-    })/* jsonデータを取り込んだときの動作ここまで */
+      `);
+
+
+    }) /* jsonデータを取り込んだときの動作ここまで */
 
     .catch((error) => console.error("Error fetching JSON:", error));
 });
@@ -207,7 +252,7 @@ function displayPoem(index) {
 ////////////////////////////////////////////////////////////
 //次へボタンの実装
 $(function () {
-  $('#next').on('click', function (event) {
+  $("#next").on("click", function (event) {
     const selectMenu = document.getElementById("jsonSelector");
     // 現在の選択肢を取得する
     let currentOptionIndex = selectMenu.selectedIndex;
@@ -219,7 +264,7 @@ $(function () {
     }
     // セレクトメニューを次の選択肢に設定する
     selectMenu.selectedIndex = nextOptionIndex;
-    const changeEvent = new Event('change');
+    const changeEvent = new Event("change");
     selectMenu.dispatchEvent(changeEvent);
     //「歌人を選んでください」のoption要素を削除する
     removeOptionByValue(jsonSelector, "all");
@@ -232,7 +277,7 @@ $(function () {
 //前へボタンの実装
 
 $(function () {
-  $('#back').on('click', function (event) {
+  $("#back").on("click", function (event) {
     //「歌人を選んでください」のoption要素を削除する
     removeOptionByValue(jsonSelector, "all");
     // セレクトメニュー要素を取得します
@@ -247,7 +292,7 @@ $(function () {
     }
     // セレクトメニューを前の選択肢に設定します
     selectMenu.selectedIndex = previousOptionIndex;
-    const changeEvent = new Event('change');
+    const changeEvent = new Event("change");
     selectMenu.dispatchEvent(changeEvent);
     event.stopPropagation();
   });
@@ -256,19 +301,16 @@ $(function () {
 /* 前へボタン、戻るボタンの誤動作を防止するため、
 ダブルクリックの親要素への伝播をstopさせる */
 $(function () {
-  $('#back').on('click', function (event) {
+  $("#back").on("click", function (event) {
     event.stopPropagation();
   });
 });
 $(function () {
-  $('#next').on('click', function (event) {
+  $("#next").on("click", function (event) {
     event.stopPropagation();
   });
 });
 /* 親要素への伝播をstopさせる　ここまで */
-
-
-
 
 fetch(jsonAddress)
   .then((response) => response.json())
@@ -287,22 +329,26 @@ fetch(jsonAddress)
     themeSelector.value = desiredTheme;
 
     // 'change'イベントを新しく作成します
-    const changeEvent = new Event('change');
+    const changeEvent = new Event("change");
 
     // themeSelector上で'change'イベントを発生させます
     themeSelector.dispatchEvent(changeEvent);
+
+    // Initialize the page with the first poem
+    initializePageWithFirstPoem();
   })
   .catch((error) => {
-    console.error('データの取得に失敗!:', error);
+    console.error("データの取得に失敗!:", error);
   });
-
 
 ////////////////////////////////////////////////////////////
 // option要素「歌人を選んでください」を削除する関数
 ////////////////////////////////////////////////////////////
 function removeOptionByValue(selector, valueToRemove) {
   // selectorから指定した値のoption要素を取得します
-  const optionToRemove = selector.querySelector(`option[value = "${valueToRemove}"]`);
+  const optionToRemove = selector.querySelector(
+    `option[value = "${valueToRemove}"]`
+  );
   // option要素が存在する場合に削除します
   if (optionToRemove) {
     selector.removeChild(optionToRemove);
@@ -312,23 +358,23 @@ function removeOptionByValue(selector, valueToRemove) {
 ////////////////////////////////////////////////////////////
 // 見出しをクリックしてリロード
 ////////////////////////////////////////////////////////////
-$('h1').on('click', function () {
+$("h1").on("click", function () {
   location.reload();
 });
 
 ////////////////////////////////////////////////////////////
 // ハンバーガーメニュー
 ////////////////////////////////////////////////////////////
-$('#burger').on('click', function () {
-  $(this).toggleClass('active');
-  $('#sp-menu').toggleClass('drawer');
+$("#burger").on("click", function () {
+  $(this).toggleClass("active");
+  $("#sp-menu").toggleClass("drawer");
 });
 
-$('#close').on('click', function () {
-  $('#burger').removeClass('active');
-  $('#sp-menu').toggleClass('drawer');
+$("#close").on("click", function () {
+  $("#burger").removeClass("active");
+  $("#sp-menu").toggleClass("drawer");
   clearSearch();
-})
+});
 ////////////////////////////////////////////////////////////
 // sp-menu
 ////////////////////////////////////////////////////////////
@@ -338,27 +384,38 @@ fetch(jsonAddress)
   .then((response) => response.json())
   .then((data) => {
     // テーブル要素を作成
-    let table = document.createElement('table');
+    let table = document.createElement("table");
     // table.id = 'myTable'; // テーブルのIDを設定
-
 
     // テーブルの各行を生成
     for (let key in data) {
       let poem = data[key];
       // tr要素を生成
-      let tr = document.createElement('tr');
+      let tr = document.createElement("tr");
 
       // number, first, nameの情報をセルに追加
-      let numberTd = document.createElement('td');
+      let numberTd = document.createElement("td");
       numberTd.textContent = poem.number;
       tr.appendChild(numberTd);
 
-      let wakaTd = document.createElement('td');
-      wakaTd.innerHTML = poem.first.replace(/<br\s*\/?>/g, " ") + '<br>' + poem.second.replace(/<br\s*\/?>/g, " ") + '<span class="small">' + poem.name + '（' + poem.date + '）' + '</span>';
+      let wakaTd = document.createElement("td");
+      wakaTd.innerHTML =
+        poem.first.replace(/<br\s*\/?>/g, " ") +
+        "<br>" +
+        poem.second.replace(/<br\s*\/?>/g, " ") +
+        '<span class="small">' +
+        poem.name +
+        "（" +
+        poem.date +
+        "）" +
+        "</span>";
       tr.appendChild(wakaTd);
 
-      let kanaTd = document.createElement('td');
-      kanaTd.innerHTML = poem.yomihuda.replace(/<rt>.*?<\/rt>/g, '');/* rt要素を削除する */;
+      let kanaTd = document.createElement("td");
+      kanaTd.innerHTML = poem.yomihuda.replace(
+        /<rt>.*?<\/rt>/g,
+        ""
+      ); /* rt要素を削除する */
       tr.appendChild(kanaTd);
 
       // tr要素をテーブルに追加
@@ -366,29 +423,28 @@ fetch(jsonAddress)
     }
 
     // 生成したtable要素をHTMLの要素（idが'dbTable'の要素）に追加する
-    document.getElementById('table').appendChild(table);
+    document.getElementById("table").appendChild(table);
   })
   .catch((error) => console.error("Error fetching JSON:", error));
-
 
 ////////////////////////////////////////////////////////////
 // 検索機能
 ////////////////////////////////////////////////////////////
-let searchInput = document.getElementById('searchInput');
+let searchInput = document.getElementById("searchInput");
 let tableRows;
 
-searchInput.addEventListener('keyup', () => {
+searchInput.addEventListener("keyup", () => {
   if (table && !tableRows) {
-    tableRows = table.getElementsByTagName('tr');
+    tableRows = table.getElementsByTagName("tr");
   }
 
   let searchValue = searchInput.value.toLowerCase();
   for (let i = 0; i < tableRows.length; i++) {
     let rowText = tableRows[i].textContent.toLowerCase();
     if (rowText.includes(searchValue)) {
-      tableRows[i].style.display = '';
+      tableRows[i].style.display = "";
     } else {
-      tableRows[i].style.display = 'none';
+      tableRows[i].style.display = "none";
     }
   }
 });
@@ -401,41 +457,40 @@ function clearSearch() {
   }
 
   // 検索欄の値を空にする
-  searchInput.value = '';
+  searchInput.value = "";
 
   // テーブルの全ての行を表示する
   for (let i = 0; i < tableRows.length; i++) {
-    tableRows[i].style.display = '';
+    tableRows[i].style.display = "";
   }
 }
-
-
 
 ////////////////////////////////////////////////////////////
 // 番号をクリックしたときのリンク処理
 ////////////////////////////////////////////////////////////
-document.addEventListener('click', function (event) {
-  if (event.target.tagName === 'TD' && event.target.parentNode.firstChild === event.target) {
+document.addEventListener("click", function (event) {
+  if (
+    event.target.tagName === "TD" &&
+    event.target.parentNode.firstChild === event.target
+  ) {
     let linkNumber = parseInt(event.target.textContent.trim());
-    console.log('クリックされた数字:', linkNumber);
-    themeSelector.value = 'all';
-    themeSelector.dispatchEvent(new Event('change'));
+    console.log("クリックされた数字:", linkNumber);
+    themeSelector.value = "all";
+    themeSelector.dispatchEvent(new Event("change"));
     jsonSelector.value = linkNumber;
-    jsonSelector.dispatchEvent(new Event('change'));
-    $('#sp-menu').removeClass('drawer');
-    $('#burger').removeClass('active');
-    $('body, html').animate({ scrollTop: 0 }, 500);
+    jsonSelector.dispatchEvent(new Event("change"));
+    $("#sp-menu").removeClass("drawer");
+    $("#burger").removeClass("active");
+    $("body, html").animate({ scrollTop: 0 }, 500);
     clearSearch();
   }
 });
-
-
 
 ////////////////////////////////////////////////////////////
 // トップに戻る
 ////////////////////////////////////////////////////////////
 $(function () {
-  var pagetop = $('#page-top');
+  var pagetop = $("#page-top");
   pagetop.hide();
   $(window).scroll(function () {
     if ($(this).scrollTop() > 100) {
@@ -445,7 +500,14 @@ $(function () {
     }
   });
   pagetop.click(function () {
-    $('body, html').animate({ scrollTop: 0 }, 500);
+    $("body, html").animate({ scrollTop: 0 }, 500);
     return false;
   });
 });
+
+function initializePageWithFirstPoem() {
+  const firstPoemNumber = 1; // Assuming the first poem has the number 1
+  jsonSelector.value = firstPoemNumber;
+  const changeEvent = new Event("change");
+  jsonSelector.dispatchEvent(changeEvent);
+}
